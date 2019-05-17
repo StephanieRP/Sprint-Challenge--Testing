7754b21cd3c278b4ab1ebee0c71dd2b1af131dbf
Stephanie Patton <stephaniepatton@Stephanies-MacBook-Air.local>
2019-05-17 13:05:33 -0500

    updated datebase

--- data/migrations/20190517094753_games.js
+++ data/migrations/20190517094753_games.js
@@ -2,10 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable("games", tbl => {
     tbl.increments();
 
-    tbl
-      .string("title", 128)
-      .notNullable()
-      .unique();
+    tbl.string("title", 128).notNullable();
 
     tbl.string("genre", 128).notNullable();
     tbl.integer("releaseYear");
